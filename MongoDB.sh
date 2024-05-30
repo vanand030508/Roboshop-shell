c171437f8812882a86aae913cf869e0f6c9366a4
AnandReddy <anandreddy8086@gmail.com>
2024-05-30 14:34:10 +0530

    roboshop shell script

--- MongoDB.sh
+++ MongoDB.sh
@@ -29,10 +29,27 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-cp Mongo.repo /etc/yum.repos.d/Mongo.repo &>> $LOGFILE
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copied MongoDB Repo"
 
-yum install mongodb-org -y &>> $LOGFILE
+dnf install mongodb-org -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "Enabling MongoDB"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "Starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Remote access to MongoDB"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "Restarting MongoDB"
+
