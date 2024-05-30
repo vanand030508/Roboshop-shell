93a217682bf2e29f8d91b56c0ba61aaba9d828b8
AnandReddy <anandreddy8086@gmail.com>
2024-05-30 14:35:17 +0530

    roboshop shell script

--- MongoDB.sh
+++ MongoDB.sh
@@ -29,7 +29,7 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+cp Mongo.repo /etc/yum.repos.d/Mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copied MongoDB Repo"
 
